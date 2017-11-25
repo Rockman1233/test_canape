59f4688c36ab68e0fe342a1080669ff24ef5aeeb
Rockman1233 <rocki67@me.com>
2017-11-25 16:17:20 +0300

    footter

---
+++ controllers/AdminController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergejandrejkin
+ * Date: 24.11.17
+ * Time: 22:20
+ */
---
+++ controllers/MainController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergejandrejkin
+ * Date: 24.11.17
+ * Time: 22:20
+ */
+
+class IndexController extends Controller {
+
+};
+
